0c0ac0817f7fdd8dcf9bc5d666982a2424510fd9
BrettScheepers <brettscheepers3100@gmail.com>
2021-09-24 21:05:48 +0200

    Incorporating document for material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,48 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+// import '../styles/globals.css'
+// import Layout from '../components/Layout'
 
-function MyApp({ Component, pageProps }) {
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <Layout>
+//       <Component {...pageProps} />
+//     </Layout>
+//   )
+// }
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import Layout from '../components/Layout'
+import CssBaseline from '@mui/material/CssBaseline';
+import '../styles/globals.css';
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Layout>
+        {<Component {...pageProps} />}
+      </Layout>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
