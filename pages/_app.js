32ca1466a8686f8767f7ceb9a7ee401be61892d1
BrettScheepers <brettscheepers3100@gmail.com>
2021-09-26 20:47:19 +0200

    Complete Front End Add Res

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import Layout from '../components/Layout'
+import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline';
 import '../styles/globals.css';
+import theme from '../styles/theme'
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -33,11 +35,13 @@ export default function MyApp(props) {
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <Layout>
-        {<Component {...pageProps} />}
-      </Layout>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Layout>
+          {<Component {...pageProps} />}
+        </Layout>
+      </ThemeProvider>
     </React.Fragment>
   );
 }
