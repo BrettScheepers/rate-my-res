0d6bfb86a7276969c7184eb47720f167bf9578b5
BrettScheepers <brettscheepers3100@gmail.com>
2021-10-02 01:35:16 +0200

    Halfway done with residence front end

--- pages/universities/[slugUni]/add-res.js
+++ pages/universities/[slugUni]/add-res.js
@@ -9,82 +9,82 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import moment from 'moment'
-// import {amenitiesDefaultArr} from '../../../imports/imports'
+import {amenitiesDefaultArr} from '../../../imports/imports'
 
 // Icons
-import WcIcon from '@mui/icons-material/Wc'; //Private Bathroom
-import FastfoodIcon from '@mui/icons-material/Fastfood'; //Dining Hall
-import WifiIcon from '@mui/icons-material/Wifi'; //Wifi
-import LocalLaundryServiceIcon from '@mui/icons-material/LocalLaundryService'; //Laundry
-import AcUnitIcon from '@mui/icons-material/AcUnit'; //Air Cond
-import KitchenIcon from '@mui/icons-material/Kitchen'; //Kitchen
-import WeekendIcon from '@mui/icons-material/Weekend'; //Student Area
-import ElevatorIcon from '@mui/icons-material/Elevator'; //Elevator
-import RunCircleIcon from '@mui/icons-material/RunCircle'; //Gym
-import EmailIcon from '@mui/icons-material/Email'; //Mailroom
+// import WcIcon from '@mui/icons-material/Wc'; //Private Bathroom
+// import FastfoodIcon from '@mui/icons-material/Fastfood'; //Dining Hall
+// import WifiIcon from '@mui/icons-material/Wifi'; //Wifi
+// import LocalLaundryServiceIcon from '@mui/icons-material/LocalLaundryService'; //Laundry
+// import AcUnitIcon from '@mui/icons-material/AcUnit'; //Air Cond
+// import KitchenIcon from '@mui/icons-material/Kitchen'; //Kitchen
+// import WeekendIcon from '@mui/icons-material/Weekend'; //Student Area
+// import ElevatorIcon from '@mui/icons-material/Elevator'; //Elevator
+// import RunCircleIcon from '@mui/icons-material/RunCircle'; //Gym
+// import EmailIcon from '@mui/icons-material/Email'; //Mailroom
 
-export const amenitiesDefaultArr = [
-    {
-        data: "bathroom",
-        text: "Private Bathroom",
-        icon: <WcIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "dining",
-        text: "Dining Hall",
-        icon: <FastfoodIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "wifi",
-        text: "Wifi",
-        icon: <WifiIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "laundry",
-        text: "Laundry",
-        icon: <LocalLaundryServiceIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "airconditioning",
-        text: "Air Conditioning",
-        icon: <AcUnitIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "kitchen",
-        text: "Kitchen",
-        icon: <KitchenIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "studentarea",
-        text: "Student Area",
-        icon: <WeekendIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "elevator",
-        text: "Elevator",
-        icon: <ElevatorIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "gym",
-        text: "Gym",
-        icon: <RunCircleIcon fontSize="large" />,
-        active: false,
-    },
-    {
-        data: "mail",
-        text: "Mailing Facilities",
-        icon: <EmailIcon fontSize="large" />,
-        active: false,
-    }
-]
+// export const amenitiesDefaultArr = [
+//     {
+//         data: "bathroom",
+//         text: "Private Bathroom",
+//         icon: <WcIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "dining",
+//         text: "Dining Hall",
+//         icon: <FastfoodIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "wifi",
+//         text: "Wifi",
+//         icon: <WifiIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "laundry",
+//         text: "Laundry",
+//         icon: <LocalLaundryServiceIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "airconditioning",
+//         text: "Air Conditioning",
+//         icon: <AcUnitIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "kitchen",
+//         text: "Kitchen",
+//         icon: <KitchenIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "studentarea",
+//         text: "Student Area",
+//         icon: <WeekendIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "elevator",
+//         text: "Elevator",
+//         icon: <ElevatorIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "gym",
+//         text: "Gym",
+//         icon: <RunCircleIcon fontSize="large" />,
+//         active: false,
+//     },
+//     {
+//         data: "mail",
+//         text: "Mailing Facilities",
+//         icon: <EmailIcon fontSize="large" />,
+//         active: false,
+//     }
+// ]
 
 
 
--- pages/universities/[slugUni]/residence/index.js
+++ pages/universities/[slugUni]/residence/index.js
@@ -1,10 +1,84 @@
-import React from 'react'
+// import { pool } from '../../../../database/db'
+import {useState} from 'react'
+import Image from 'next/image'
+import Header from '../../../../components/Header'
+import BackButton from '../../../../components/BackButton'
+import Rating from '@mui/material/Rating';
+import Button from '@mui/material/Button';
+import {amenitiesDefaultArr} from '../../../../imports/imports'
+
 
 const Residence = () => {
+    const [amenitySpec, setAmenitySpec] = useState(amenitiesDefaultArr.slice(0,5))
+
     return (
-        <div>
-            
-        </div>
+        <>  
+            <Header />
+            <div className="residence-page">
+                <div className="name">
+                    <BackButton />
+                    {/* Res Name */}
+                    <h2>Smuts Hall</h2>
+                </div>
+                <div className="res-banner">
+                    <div className="content">
+                        <h3 style={{fontWeight: 500}}>Overall Quality Rating</h3>
+                        {/* Res Overall Rating */}
+                        <h1>4.5</h1>
+                        <Rating name="read-only" size="large" value={4.5} precision={0.25} readOnly />
+                        <button className="btn-main">Write A Review</button>
+                        {/* <Button variant="contained" >Write a Review</Button> */}
+                    </div>
+                    <div className="image">
+                        {/* Res Image */}
+                        <Image 
+                            src="/assets/uni-images/uct.jpg"
+                            alt="uni"
+                            layout="fill"
+                            objectFit="cover"
+                        />
+                    </div>
+                </div>
+                <div className="res-content">
+                    <div className="res-specs">
+                        <div className="spacing">
+                            <h5>Rating Breakdown</h5>
+                            <div className="rating-spec">
+                                <div>Room - <span className="bold">4.5</span></div>
+                                <span><Rating name="read-only" value={4.5} precision={0.25} readOnly /></span>
+                            </div>
+                            <div className="rating-spec">
+                                <div>Bathroom - <span className="bold">4.5</span></div>
+                                <span><Rating name="read-only" value={4.5} precision={0.25} readOnly /></span>
+                            </div>
+                            <div className="rating-spec">
+                                <div>Building - <span className="bold">4.5</span></div>
+                                <span><Rating name="read-only" value={4.5} precision={0.25} readOnly /></span>
+                            </div>
+                            <div className="rating-spec">
+                                <div>Location - <span className="bold">4.5</span></div>
+                                <span><Rating name="read-only" value={4.5} precision={0.25} readOnly /></span>
+                            </div>
+                        </div>
+
+                        <div className="spacing">
+                            <h5>Amenities</h5>
+                            {
+                                amenitySpec.map(el => {
+                                    const {icon, text, data} = el
+
+                                    return <div key={data} className="amenity-spec">
+                                        {icon}
+                                        <span>{text}</span>
+                                    </div>
+                                })
+                            }
+                        </div>
+                    </div>
+                    <div className="reviews"></div>
+                </div>
+            </div>
+        </>
     )
 }
 
