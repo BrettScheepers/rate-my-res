a6ec67e6fe33278996a58c112fa929219df6c0d2
BrettScheepers <brettscheepers3100@gmail.com>
2021-10-08 16:04:04 +0200

    Attempted Database Fix

--- database/db.js
+++ database/db.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg')
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: !isProduction ? isProduction : { rejectUnauthorized: false }
 })
 
 export { pool }
