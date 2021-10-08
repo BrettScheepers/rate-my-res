7aed739ab4af45b6390950a3cc5ab1e5ecf6a3e9
BrettScheepers <brettscheepers3100@gmail.com>
2021-10-08 16:20:17 +0200

    Attempted database error fix 2

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg')
 
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -8,7 +8,7 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: !isProduction ? isProduction : { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false }
 })
 
 export { pool }
