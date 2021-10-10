3c8e3b9f48d34a87e22eed9781d79bf1222dcac4
BrettScheepers <brettscheepers3100@gmail.com>
2021-10-10 21:20:53 +0200

    Revert to old database

--- database/db.js
+++ database/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }
+    // ssl: { rejectUnauthorized: false }
 })
 
 export { pool }
