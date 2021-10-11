e035134f097a23070c92fe243e4744e5648dad2a
BrettScheepers <brettscheepers3100@gmail.com>
2021-10-12 00:00:58 +0200

    Merged 2

--- config/index.js
+++ config/index.js
@@ -1,7 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-<<<<<<< HEAD
 export const server = dev ? 'http://localhost:3000' : 'https://www.ratemyres.co.za'
-=======
-export const server = dev ? 'http://localhost:3000' : 'https://rate-my-res.vercel.app/'
->>>>>>> e9029374408c9ebaaac5ca77a7c575ffcdf7cfb2
