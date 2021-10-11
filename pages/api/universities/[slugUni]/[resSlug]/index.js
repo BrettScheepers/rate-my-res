cc22d62976bf62e9399f849de5d06b32dc723649
BrettScheepers <brettscheepers3100@gmail.com>
2021-10-11 22:16:35 +0200

    Added cors headers next config 2

--- pages/api/universities/[slugUni]/[resSlug]/index.js
+++ pages/api/universities/[slugUni]/[resSlug]/index.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     await NextCors(req, res, {
         // Options
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-        origin: ['https://www.ratemyres.co.za/', 'http://localhost:3000'],
+        origin: '*',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
 
