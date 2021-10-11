e9029374408c9ebaaac5ca77a7c575ffcdf7cfb2
BrettScheepers <brettscheepers3100@gmail.com>
2021-10-11 23:37:17 +0200

    Added cors headers next config 6

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://ratemyres.co.za'
+export const server = dev ? 'http://localhost:3000' : 'https://rate-my-res.vercel.app/'
